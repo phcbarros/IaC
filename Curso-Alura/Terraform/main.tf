bff9fa8edefab839c202c801c687a249210fb93f
phcbarros <paulo.barros@outlook.com.br>
2023-07-27 18:20:48 -0300

    Curso Alura - Terraform - 02 Modificando a infraestrutura

--- Curso-Alura/Terraform/main.tf
+++ Curso-Alura/Terraform/main.tf
@@ -10,4 +10,33 @@ resource "aws_instance" "app_server" {
   tags = {
     Name = "dev-${count.index}"
   }
+
+  vpc_security_group_ids = [""]
+}
+
+resource "aws_security_group" "acesso-ssh" {
+  name        = "acesso-ssh"
+  description = "acesso-ssh"
+  #vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "Acesso SSH"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = [""]
+    #ipv6_cidr_blocks = [""]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "ssh"
+  }
 }
