333d66f9b18ee63c17a50710efda5877128316c7
phcbarros <paulo.barros@outlook.com.br>
2023-07-31 13:01:16 -0300

    Curso Alura - Terraform - 03 Utilizando referências e dependências entre recursos

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -2,18 +2,6 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "app_server" {
-  count = 3
-  ami           = "ami-0f34c5ae932e6f0e4"
-  instance_type = "t2.micro"
-  key_name = "iac-alura"
-  tags = {
-    Name = "dev-${count.index}"
-  }
-
-  vpc_security_group_ids = [""]
-}
-
 resource "aws_security_group" "acesso-ssh" {
   name        = "acesso-ssh"
   description = "acesso-ssh"
@@ -39,4 +27,62 @@ resource "aws_security_group" "acesso-ssh" {
   tags = {
     Name = "ssh"
   }
+}
+
+resource "aws_instance" "dev" {
+  count = 3
+  ami           = "ami-0f34c5ae932e6f0e4"
+  instance_type = "t2.micro"
+  key_name = "iac-alura"
+  tags = {
+    Name = "dev-${count.index}"
+  }
+
+  vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
+}
+
+resource "aws_instance" "dev4" {
+  ami           = "ami-0f34c5ae932e6f0e4"
+  instance_type = "t2.micro"
+  key_name = "iac-alura"
+  tags = {
+    Name = "dev4"
+  }
+
+  vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
+  depends_on = [ aws_instance.dev4 ]
+}
+
+resource "aws_instance" "dev5" {
+  ami           = "ami-0f34c5ae932e6f0e4"
+  instance_type = "t2.micro"
+  key_name = "iac-alura"
+  tags = {
+    Name = "dev5"
+  }
+
+  vpc_security_group_ids = ["${aws_security_group.acesso-ssh.id}"]
+}
+
+
+resource "aws_s3_bucket" "bucket-dev4" {
+  bucket = "phlabs-dev4"
+
+  tags = {
+    Name        = "labs-dev4"
+  }
+}
+
+resource "aws_s3_bucket_ownership_controls" "dev4" {
+  bucket = aws_s3_bucket.bucket-dev4.id
+  rule {
+    object_ownership = "BucketOwnerPreferred"
+  }
+}
+
+resource "aws_s3_bucket_acl" "dev4" {
+  depends_on = [aws_s3_bucket_ownership_controls.dev4]
+
+  bucket = aws_s3_bucket.bucket-dev4.id
+  acl    = "private"
 }
